ab3e733719d2131a093e5783e44dad6480a161ca
Krzysztof Kijanowski <k12kijanowski@gmail.com>
2023-02-10 19:23:21 +0100

    Tweaks to the tests

--- sources/math.c
+++ sources/math.c
@@ -1,12 +1,13 @@
 #include "module.h"
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
     double num1 = 10.0;
     double num2 = 3.14;
-    (void) add(num1,num2);
-    (void) substract(num1, num2);
-    (void) multiply(num1,num2);
-    (void) divide(num1,num2);
+    double result;
+    result = add(num1,num2);
+    result = substract(num1, num2);
+    result = multiply(num1,num2);
+    result = divide(num1,num2);
     return 0;
 }
--- tests/uTests.c
+++ tests/uTests.c
@@ -7,12 +7,13 @@
 #define FAIL   -1
 #define NOT_EXECUTED    0
 
-
+/* Variables for holding values of each test case for function add() */
 unsigned short add_testVerdict[NUM_OF_TCS];
 double add_val1[NUM_OF_TCS]     = {-DBL_MAX,    -1.0,    0.0,   1.0,    DBL_MAX,    -1.0,               1.0,    1.0,    1.0, 1.0};
 double add_val2[NUM_OF_TCS]     = {-1.0,            1.0,     1.0,   1.0,    1.0,        -DBL_MAX,       -1.0,   0.0,    1.0, DBL_MAX};
 double add_retVal[NUM_OF_TCS]   = {-DBL_MAX,    0.0,     1.0,   2.0,    DBL_MAX,    -DBL_MAX,       0.0,    1.0,    2.0, DBL_MAX};
 
+/* Variables for holding values of each test case for function divide() */
 double divide_val1[NUM_OF_TCS] = {-DBL_MAX, -1.0, 0.0, 1.0, DBL_MAX, 1.0, 1.0, 1.0, 1.0};
 double divide_val2[NUM_OF_TCS] = {1.0, 1.0, 1.0, 1.0, 1.0, -DBL_MAX, -1.0, 0.0, DBL_MAX};
 double divide_retVal[NUM_OF_TCS] = {-DBL_MAX, -1.0, 0.0, 1.0, DBL_MAX, -0.0, -1.0, 0.0, 0.0};
@@ -20,20 +21,29 @@ unsigned short divide_testVerdict[NUM_OF_TCS];
 
 int main(void)
 {
-    printf("Module: %s\n", "add");
-    for (unsigned short i = 0; i < NUM_OF_TCS; i++)
+    unsigned short fails = 0;
+
+    fputs("Starting tests for file: \"module.h\"\n", stdout);
+
+    /* Test function add() */
+    printf("Function: %s\n", "add()");
+    for (unsigned short i = 0; i < NUM_OF_TCS; i++) 
     {
         double retval = 0.0;
         retval = add(add_val1[i],add_val2[i]);
-        if (retval == add_retVal[i]) add_testVerdict[i] = PASS;
+        if (retval >= (add_retVal[i] - epsilon) && (retval <= (add_retVal[i] + epsilon))) add_testVerdict[i] = PASS;
         else 
         {
             add_testVerdict[i] = FAIL;
+            fails += 1;
             printf("Testcase: %d\n\tReceived: %f\n\tExpected: %f\n", i+1, retval, add_retVal[i]);
         }
     }
+    printf("Test completed. %d / %d tests passed. %d tests failed.\n\n", NUM_OF_TCS-fails, NUM_OF_TCS, fails);
     
-    printf("Module: %s\n", "divide");
+    /* Test function divide() */
+    fails = 0;
+    printf("Function: %s\n", "divide()");
     for (unsigned short i = 0; i < NUM_OF_TCS; i++)
     {
         double retval = 0.0;
@@ -42,9 +52,11 @@ int main(void)
         else 
         {
             divide_testVerdict[i] = FAIL;
+            fails += 1;
             printf("Testcase: %d\n\tReceived: %f\n\tExpected: %f\n", i+1, retval, divide_retVal[i]);
         }
     }
+    printf("Test completed. %d / %d tests passed. %d tests failed.\n\n", NUM_OF_TCS-fails, NUM_OF_TCS, fails);
 
     return 0;
 }
